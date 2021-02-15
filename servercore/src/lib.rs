8ad192601d706bcb4dc399312d4cf0421b4b7e96
JotaPe <jotape@protonmail.com>
2021-02-15 00:21:20 -0300

    maintanence commit

--- servercore/src/lib.rs
+++ servercore/src/lib.rs
@@ -1,12 +1,16 @@
+use rusty_money::{iso, Money};
 use serde::{Deserialize, Serialize};
+use url::{ParseError, Url};
 use uuid::Uuid;
 use validator::{validate_email, validate_phone};
+use rayon::prelude::*;
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 pub struct User {
     pub user_id: String, // UUID v4
     pub email: String,
     pub phone: String,
+    pub address: String,
     pub image_url: String,
     pub password_hash: String,
     pub confirmed: bool,
@@ -18,6 +22,7 @@ impl User {
         user_id: &str,
         email: &str,
         phone: &str,
+        address: &str,
         image_url: &str,
         password_hash: &str,
         confirmed: bool,
@@ -27,10 +32,11 @@ impl User {
             user_id: user_id.to_string(),
             email: email.to_string(),
             phone: phone.to_string(),
+            address: address.to_string(),
             image_url: image_url.to_string(),
             password_hash: password_hash.to_string(),
             confirmed,
-            roles: roles.iter().map(|&val| val.to_string()).collect(),
+            roles: roles.par_iter().map(|&val| val.to_string()).collect(),
         }
     }
 
@@ -38,8 +44,10 @@ impl User {
         user_id: &str,
         email: &str,
         phone: &str,
+        address: &str,
         image_url: &str,
         password_hash: &str,
+        confirmed: bool,
         roles: Vec<&str>,
     ) -> Result<User, &'static str> {
         match Uuid::parse_str(user_id) {
@@ -54,26 +62,68 @@ impl User {
             true => true,
             false => return Err("Phone is invalid"),
         };
+        match Url::parse(image_url) {
+            Ok(url) => url,
+            Err(_) => return Err("URL is not valid"),
+        };
         Ok(User::new(
             user_id,
             email,
             phone,
+            address,
             image_url,
             password_hash,
-            false,
+            confirmed,
             roles,
         ))
     }
+
+    pub fn update_email(&self, email: &str) -> Result<User, &'static str> {
+        match validate_email(email) {
+            true => true,
+            false => return Err("Email is not valid"),
+        };
+        Ok(User {
+            email: email.to_string(),
+            ..self.clone()
+        })
+    }
+
+    // not tested functions
+    pub fn update_phone(&self, phone: &str) -> Result<User, &'static str> {
+        match validate_phone(phone) {
+            true => true,
+            false => return Err("Phone number is not valid"),
+        };
+        Ok(User {
+            phone: phone.to_string(),
+            ..self.clone()
+        })
+    }
+
+    // not tested functions
+    pub fn update_address(&self, address: &str) -> Result<User, &'static str> {
+        Ok(User {
+            address: address.to_string(),
+            ..self.clone()
+        })
+    }
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 pub struct Product {
     pub product_id: String,  // UUID v4
     pub business_id: String, // UUID v4
     pub title: String,
     pub description: String,
     pub image_url: String,
-    pub tags: Vec<String>,
+    // We're storing as a String to be more usable in the future
+    pub price: String,
+    // unformatted_price is just to store the original input
+    // to be used on the future for making a better formatted
+    // version of price.
+    pub unformatted_price: String,
+    pub product_tags: Vec<String>,
 }
 
 impl Product {
@@ -83,7 +133,9 @@ impl Product {
         title: &str,
         description: &str,
         image_url: &str,
-        tags: Vec<&str>,
+        price: &str,
+        unformatted_price: &str,
+        product_tags: Vec<&str>,
     ) -> Product {
         Product {
             product_id: product_id.to_string(),
@@ -91,7 +143,9 @@ impl Product {
             title: title.to_string(),
             description: description.to_string(),
             image_url: image_url.to_string(),
-            tags: tags.iter().map(|&val| val.to_string()).collect(),
+            price: price.to_string(),
+            unformatted_price: unformatted_price.to_string(),
+            product_tags: product_tags.par_iter().map(|&val| val.to_string()).collect(),
         }
     }
 
@@ -101,7 +155,8 @@ impl Product {
         title: &str,
         description: &str,
         image_url: &str,
-        tags: Vec<&str>,
+        unformatted_price: &str,
+        product_tags: Vec<&str>,
     ) -> Result<Product, &'static str> {
         match Uuid::parse_str(product_id) {
             Ok(uuid_id) => uuid_id,
@@ -111,6 +166,10 @@ impl Product {
             Ok(uuid_id) => uuid_id,
             Err(_) => return Err("Business UUID is not valid"),
         };
+        match Url::parse(image_url) {
+            Ok(url) => url,
+            Err(_) => return Err("URL is not valid"),
+        };
         if title.len() < 5 {
             return Err("Too short title");
         } else if title.len() > 100 {
@@ -118,35 +177,58 @@ impl Product {
         }
         if description.len() < 5 {
             return Err("Too short description");
-        } else if title.len() > 500 {
+        } else if title.len() > 1000 {
             return Err("Too big description");
         }
+        let price = Money::from_str(unformatted_price, iso::BRL)
+            .unwrap()
+            .to_string();
         Ok(Product::new(
             product_id,
             business_id,
             title,
             description,
             image_url,
-            tags,
+            &price,
+            unformatted_price,
+            product_tags,
         ))
     }
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 pub struct Business {
-    pub user_id: String, // UUID v4
-    pub products_id: Vec<String>,
+    pub business_id: String,      // UUID v4
+    pub user_id: String,          // UUID v4
+    pub products_id: Vec<String>, // Vector of UUIDs v4
+    pub business_tags: Vec<String>,
 }
 
 impl Business {
-    pub fn new(user_id: &str, products_id: Vec<&str>) -> Business {
+    pub fn new(
+        business_id: &str,
+        user_id: &str,
+        products_id: Vec<&str>,
+        business_tags: Vec<&str>,
+    ) -> Business {
         Business {
+            business_id: business_id.to_string(),
             user_id: user_id.to_string(),
-            products_id: products_id.iter().map(|&val| val.to_string()).collect(),
+            products_id: products_id.par_iter().map(|&val| val.to_string()).collect(),
+            business_tags: business_tags.par_iter().map(|&val| val.to_string()).collect(),
         }
     }
 
-    pub fn create(user_id: &str, products_id: Vec<&str>) -> Result<Business, &'static str> {
+    pub fn create(
+        business_id: &str,
+        user_id: &str,
+        products_id: Vec<&str>,
+        business_tags: Vec<&str>,
+    ) -> Result<Business, &'static str> {
+        match Uuid::parse_str(business_id) {
+            Ok(uuid_id) => uuid_id,
+            Err(_) => return Err("A ID is not a UUID"),
+        };
         match Uuid::parse_str(user_id) {
             Ok(uuid_id) => uuid_id,
             Err(_) => return Err("A ID is not a UUID"),
@@ -157,11 +239,16 @@ impl Business {
                 Err(_) => return Err("A ID is not a UUID"),
             };
         }
-        Ok(Business::new(user_id, products_id))
+        Ok(Business::new(
+            business_id,
+            user_id,
+            products_id,
+            business_tags,
+        ))
     }
 }
 
-#[derive(Clone, Debug, Serialize, Deserialize, PartialEq)]
+#[derive(Clone, Debug, Serialize, Deserialize, PartialEq, Default)]
 pub struct Order {
     pub order_id: String,   // UUID v4
     pub product_id: String, // UUID v4
@@ -176,11 +263,12 @@ mod tests {
     use super::User;
 
     #[test]
-    fn new_user() {
+    fn test_new_user() {
         let user = User::new(
             "XD",
             "email",
             "3091u2304",
+            "dominguinhos",
             "http://image.url.com",
             "XDXD",
             false,
@@ -191,6 +279,7 @@ mod tests {
                 user_id: "XD".to_string(),
                 email: "email".to_string(),
                 phone: "3091u2304".to_string(),
+                address: "dominguinhos".to_string(),
                 image_url: "http://image.url.com".to_string(),
                 password_hash: "XDXD".to_string(),
                 confirmed: false,
@@ -202,13 +291,15 @@ mod tests {
     }
 
     #[test]
-    fn create_user() {
+    fn test_create_user() {
         let user = User::create(
             "25650673-c3e8-4cbb-a7bd-e27d268157b8",
             "email@example.com",
             "+5521965237969",
+            "Rua Dominguinhos",
             "http://image.url.com",
             "XDXD",
+            false,
             vec!["admin"],
         )
         .is_ok();
@@ -216,13 +307,32 @@ mod tests {
     }
 
     #[test]
-    fn new_product() {
+    fn update_email_user() {
+        let user = User::create(
+            "25650673-c3e8-4cbb-a7bd-e27d268157b8",
+            "email@example.com",
+            "+5521965237969",
+            "Rua Dominguinhos",
+            "http://image.url.com",
+            "XDXD",
+            false,
+            vec!["admin"],
+        )
+        .unwrap();
+        let updated_user = user.update_email("email@example2.com");
+        assert!(updated_user.is_ok(), "User email updated successfully")
+    }
+
+    #[test]
+    fn test_new_product() {
         let product = Product::new(
             "09320481032",
             "102938409128305",
             "aiowjefaieorf",
             "oiewroaer",
             "oijeaij",
+            "2000",
+            "2000",
             vec!["oaiewjf"],
         );
         assert_eq!(
@@ -233,42 +343,53 @@ mod tests {
                 title: "aiowjefaieorf".to_string(),
                 description: "oiewroaer".to_string(),
                 image_url: "oijeaij".to_string(),
-                tags: vec!["oaiewjf".to_string()]
+                price: "2000".to_string(),
+                unformatted_price: "2000".to_string(),
+                product_tags: vec!["oaiewjf".to_string()]
             }
         );
     }
 
     #[test]
-    fn create_product() {
+    fn test_create_product() {
         let product = Product::create(
             "25650673-c3e8-4cbb-a7bd-e27d268157b8",
             "25650673-c3e8-4cbb-a7bd-e27d268157b8",
             "aoeirojfiearg",
             "oaiejriearg",
-            "aoiejroear",
+            "http://image.com",
+            "2000",
             vec!["fiejroa"],
-        )
-        .is_ok();
+        ).is_ok();
         assert!(product, "Product create function is working correctly");
     }
 
     #[test]
-    fn new_business() {
-        let business = Business::new("1029340918", vec!["01239041"]);
+    fn test_new_business() {
+        let business = Business::new(
+            "25650673-c3e8-4cbb-a7bd-e27d268157b8",
+            "1029340918",
+            vec!["01239041"],
+            vec!["hamburguer"],
+        );
         assert_eq!(
             business,
             Business {
+                business_id: "25650673-c3e8-4cbb-a7bd-e27d268157b8".to_string(),
                 user_id: "1029340918".to_string(),
-                products_id: vec!["01239041".to_string()]
+                products_id: vec!["01239041".to_string()],
+                business_tags: vec!["hamburguer".to_string()],
             }
         );
     }
 
     #[test]
-    fn create_business() {
+    fn test_create_business() {
         let business = Business::create(
             "25650673-c3e8-4cbb-a7bd-e27d268157b8",
+            "25650673-c3e8-4cbb-a7bd-e27d268157b8",
             vec!["25650673-c3e8-4cbb-a7bd-e27d268157b8"],
+            vec!["hamburguer"],
         )
         .is_ok();
         assert!(business, "Business struct created");
--- users/src/main.rs
+++ users/src/main.rs
@@ -1,3 +1,107 @@
-fn main() {
-    println!("Hello, world!");
+use servercore::Business;
+use servercore::User;
+use tonic::{transport::Server, Code, Request, Response, Status};
+use users::user_service_server::{UserService, UserServiceServer};
+use users::{BusinessData, BusinessResponseData, UserData, UserResponseData};
+use uuid::Uuid;
+pub mod users {
+    tonic::include_proto!("users");
+}
+
+#[derive(Default)]
+struct UserGRPCData {}
+
+#[tonic::async_trait]
+impl UserService for UserGRPCData {
+    async fn create_user(
+        &self,
+        request: Request<UserData>,
+    ) -> Result<Response<UserResponseData>, Status> {
+        let email = &request.get_ref().email;
+        let password = &request.get_ref().password;
+        let phone = &request.get_ref().phone;
+        let address = &request.get_ref().address;
+        let image_url = &request.get_ref().image_url;
+        let roles = vec!["user"];
+        let user = match User::create(
+            &Uuid::new_v4().to_string(),
+            email,
+            phone,
+            password,
+            image_url,
+            address,
+            false,
+            roles,
+        ) {
+            Ok(user) => user,
+            Err(e) => return Err(Status::new(Code::InvalidArgument, e)),
+        };
+        Ok(Response::new(UserResponseData {
+            user_id: user.user_id,
+            email: user.email,
+            phone: user.phone,
+            address: user.address,
+            image_url: user.image_url,
+            roles: vec!["user".to_string()],
+        }))
+    }
+
+    async fn create_business(
+        &self,
+        request: Request<BusinessData>,
+    ) -> Result<Response<BusinessResponseData>, Status> {
+        let user_id = &Uuid::new_v4().to_string();
+        let business_id = &Uuid::new_v4().to_string();
+        let email = &request.get_ref().email;
+        let phone = &request.get_ref().phone;
+        let password = &request.get_ref().password;
+        let address = &request.get_ref().address;
+        let image_url = &request.get_ref().image_url;
+        let roles = vec!["business"];
+        let user = match User::create(
+            user_id,
+            email,
+            phone,
+            password,
+            image_url,
+            address,
+            false,
+            roles,
+        ) {
+            Ok(user) => user,
+            Err(e) => return Err(Status::new(Code::InvalidArgument, e)),
+        };
+        let business = match Business::create(
+            business_id,
+            user_id,
+            Vec::default(),
+            Vec::default(),
+        ) {
+            Ok(business) => business,
+            Err(e) => return Err(Status::new(Code::InvalidArgument, e)),
+        };
+        Ok(Response::new(BusinessResponseData {
+            user_id: business.user_id,
+            business_id: business.business_id,
+            email: user.email,
+            phone: user.phone,
+            address: user.address,
+            image_url: user.image_url,
+            roles: user.roles,
+        }))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse().unwrap();
+    let user_service = UserGRPCData::default();
+    println!("User service listening on {}", addr);
+
+    Server::builder()
+        .add_service(UserServiceServer::new(user_service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
